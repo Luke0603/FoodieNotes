e5ae0498b4e028942eb7694086f8f10807122d72
Luke0603 <bert0603@gmail.com>
2019-08-19 15:44:33 +0800

    Initial Commit

---
+++ FoodieNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodieNotes
+//
+//  Created by 陳博竣 on 2019/8/19.
+//  Copyright © 2019 Luke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FoodieNotesTests/FoodieNotesTests.swift
@@ -0,0 +1,34 @@
+//
+//  FoodieNotesTests.swift
+//  FoodieNotesTests
+//
+//  Created by 陳博竣 on 2019/8/19.
+//  Copyright © 2019 Luke. All rights reserved.
+//
+
+import XCTest
+@testable import FoodieNotes
+
+class FoodieNotesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
